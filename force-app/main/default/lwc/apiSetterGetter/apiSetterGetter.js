4ef680927f08343a4ae47321aedd6a5c2b3f60e5
sanarion <moristolp@hotmail.co.jp>
2020-04-16 22:06:10 +0900

    Parent-to-child end

---
+++ force-app/main/default/lwc/apiSetterGetter/apiSetterGetter.js
@@ -0,0 +1,32 @@
+import { LightningElement } from 'lwc';
+
+export default class ApiSetterGetter extends LightningElement {
+  lastTodoId = 2;
+  todos = [
+    { id: 1, description: 'Explore recipes', priority: true },
+    { id: 2, description: 'Install Ebikes sample app', priority: false }
+  ];
+
+  description;
+  priority = false;
+
+  handleDescriptionChange(event) {
+    this.description = event.target.value;
+  }
+
+  handlePriorityChange(event) {
+    this.priority = event.target.checked;
+  }
+
+  handleSave() {
+    this.lastTodoId = this.lastTodoId + 1;
+    this.todos = [
+      ...this.todos,
+      {
+        id: this.lastTodoId,
+        description:this.description,
+        priority:this.priority
+      }
+    ];
+  }
+}
