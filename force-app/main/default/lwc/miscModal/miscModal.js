599d75e2a245dd8205fe262a0b19f2c07c1ee3c5
sanarion <moristolp@hotmail.co.jp>
2020-05-11 22:36:57 +0900

    misc 1st

---
+++ force-app/main/default/lwc/miscModal/miscModal.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+
+export default class MiscModal extends LightningElement {
+  content = 'The modal content';
+  header = 'the modal header';
+
+  handleHeaderChange(event) {
+    this.header = event.target.value;
+  }
+
+  handleContentChange(event) {
+    this.content = event.target.value;
+  }
+
+  handleShowModal() {
+    const modal = this.template.querySelector('c-modal');
+    modal.show();
+  }
+
+  handleCancelModal() {
+    const modal = this.template.querySelector('c-modal');
+    modal.hide();
+  }
+
+  handleCloseModal() {
+    const modal = this.template.querySelector('c-modal');
+    modal.hide();
+  }
+
+}
---
+++ force-app/main/default/lwc/modal/modal.js
@@ -0,0 +1,45 @@
+import { LightningElement, api } from 'lwc';
+
+const CSS_CLASS = 'modal-hidden';
+
+export default class Modal extends LightningElement {
+  showModal = false;
+
+  hasHeaderString = false;
+  _headerPrivate;
+
+  @api
+  set header(value) {
+    this.hasHeaderString = value !== '';
+    this._headerPrivate = value;
+  }
+
+  get header() {
+    return this._headerPrivate;
+  }
+
+  @api show() {
+    this.showModal = true;
+  }
+
+  @api hide() {
+    this.showModal = false;
+  }
+
+  handleDialogClose() {
+    const closedialog = new CustomEvent('closedialog');
+    this.dispatchEvent(closedialog);
+    this.hide();
+  }
+
+  handleSlotTaglineChange() {
+    const taglineEl = this.template.querySelector('p');
+    taglineEl.classList.remove(CSS_CLASS);
+  }
+
+  handleSlotFooterChange() {
+    const footerEl = this.template.querySelector('footer');
+    footerEl.classList.remove(CSS_CLASS);
+  }
+
+}
